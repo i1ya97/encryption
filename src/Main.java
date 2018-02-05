0a7f9fdb5b94657933fb7abd2f4241c2c154e270
i1ya97 <31565819+i1ya97@users.noreply.github.com>
2018-02-05 21:32:31 +0700

    update

---
+++ src/Main.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        char[] alfavit = {'а','б','в','г','д','е','ё','ж','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','х','ц','ч','ш','щ','ъ','ы','ь','э','ю','я'};
+        char[] cipher=new char[33];
+        for(int i=0;i<33;i++) {
+            cipher[i]='9';
+        }
+        int j;
+        for(char i:alfavit){
+            do {
+                Random rand = new Random();
+                j = rand.nextInt(33);
+                if (cipher[j]=='9'){
+                    cipher[j]=i;
+                }
+            }while (i!=cipher[j]);
+        }
+        String text;
+        Scanner in = new Scanner(System.in);
+        text= in.nextLine();
+        text=text.toLowerCase();
+        int size = text.length();
+        char[] rezult = new char[size];
+        char [] input = text.toCharArray();
+        for(int i=0;i<size;i++){
+            boolean flag = true;
+            for(j=0;j<alfavit.length;j++){
+                if (input[i]==alfavit[j]){
+                    flag=false;
+                    rezult[i]=cipher[j];
+                }
+                else{
+                }
+            }
+            if(flag==true){
+                rezult[i]=input[i];
+            }
+        }
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream("C:/Users/ilya9/IdeaProjects/encryption/text.txt"), "UTF8"));//указать свой путь
+        for(int i=0;i<alfavit.length;i++){
+            bw.write(alfavit[i]);
+            bw.write(':');
+            bw.write(cipher[i]);
+            bw.write(", ");
+        }
+        bw.newLine();
+        for(int i=0;i<size;i++){
+            bw.write(rezult[i]);
+        }
+        bw.flush();
+        bw.close();
+    }
+}
